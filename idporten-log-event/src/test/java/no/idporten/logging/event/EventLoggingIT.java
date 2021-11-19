da24d22a6e71148db7b7655f10603c43bc1c97ed
Jan Fretheim <jan.fretheim@digdir.no>
2021-11-19 15:11:50 +0100

    Integration test with embedded cluster

---
+++ idporten-log-event/src/test/java/no/idporten/logging/event/EventLoggingIT.java
@@ -0,0 +1,110 @@
+package no.idporten.logging.event;
+
+import io.confluent.examples.streams.kafka.EmbeddedSingleNodeKafkaCluster;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+/**
+ * Integration test using the embedded Kafka cluster by Confluent
+ */
+public class EventLoggingIT {
+
+    public static final EmbeddedSingleNodeKafkaCluster cluster = new EmbeddedSingleNodeKafkaCluster();
+    public static final String TOPIC = "aktiviteter";
+    public static final long TEN_SECONDS = 10000L;
+    @TempDir
+    static File logDir;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        cluster.start(logDir);
+        cluster.createTopic(TOPIC);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        cluster.stop();
+    }
+
+    @Test
+    void shouldLog() {
+        final List<EventRecord> inputValues = Arrays.asList(
+                EventRecord.newBuilder()
+                        .setName("Innlogget")
+                        .setPid("24079409630")
+                        .setCorrelationId(UUID.randomUUID().toString())
+                        .setService("idPorten")
+                        .build(),
+                EventRecord.newBuilder()
+                        .setName("Utlogget")
+                        .setPid("24079409479")
+                        .setCorrelationId(UUID.randomUUID().toString())
+                        .setService("idPorten")
+                        .build(),
+                EventRecord.newBuilder()
+                        .setName("Endret")
+                        .setPid("24079409398")
+                        .setCorrelationId(UUID.randomUUID().toString())
+                        .setService("idPorten")
+                        .build());
+
+        EventLoggingConfig config = EventLoggingConfig.builder()
+                .bootstrapServers(cluster.bootstrapServers())
+                .schemaRegistryUrl(cluster.schemaRegistryUrl())
+                .kafkaUsername("franz")
+                .kafkaPassword("password")
+                .eventTopic(TOPIC)
+                .build();
+
+        EventLogger eventLogger = new EventLogger(config);
+
+        final Properties consumerProperties = new Properties();
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, cluster.bootstrapServers());
+        consumerProperties.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, cluster.schemaRegistryUrl());
+        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, TOPIC + "-consumer");
+        consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+
+        KafkaConsumer<String, EventRecord> consumer = new KafkaConsumer<>(consumerProperties);
+        consumer.subscribe(Collections.singleton(TOPIC));
+
+        for (EventRecord inputValue : inputValues) {
+            eventLogger.log(inputValue);
+        }
+
+        List<String> expected = inputValues.stream()
+                .map(record -> record.getPid().toString())
+                .collect(Collectors.toList());
+        List<String> received = new ArrayList<>();
+        long timeout = System.currentTimeMillis() + TEN_SECONDS;
+        while (System.currentTimeMillis() < timeout && !received.containsAll(expected)) {
+            ConsumerRecords<String, EventRecord> records = consumer.poll(Duration.ofSeconds(1));
+            records.forEach(record -> received.add(record.key()));
+        }
+
+        assertIterableEquals(expected, received);
+    }
+
+
+}
