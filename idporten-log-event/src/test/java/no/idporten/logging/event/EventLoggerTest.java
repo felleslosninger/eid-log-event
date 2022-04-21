f0190e280059c8ae4dbecbc5504879533c5992f5
Erik Dahl Østvang <erik.dahl.ostvang@digdir.no>
2022-04-21 13:30:41 +0200

    Removed Kafka key

--- idporten-log-event/src/test/java/no/idporten/logging/event/EventLoggerTest.java
+++ idporten-log-event/src/test/java/no/idporten/logging/event/EventLoggerTest.java
@@ -73,10 +73,7 @@ class EventLoggerTest {
         Future<Integer> sentEventsFuture = eventLogger.pool.submit(() -> mockProducer.history().size());
 
         assertEquals(1, sentEventsFuture.get(), "Record should be published");
-        assertEquals(
-                "Innlogget;testApplication;McDuck IT;Andeby kommune;null;OTC;unitTest;",
-                mockProducer.history().get(0).key(),
-                "Record key should be a compositeKey based upon all dimensions of the record");
+        assertNull(mockProducer.history().get(0).key(), "Record key should be null");
     }
 
     @SuppressWarnings("unchecked")
