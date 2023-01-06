1250affb5771d779dbca58889bc4a74a1c98fecf
Jan Fretheim <jan.fretheim@digdir.no>
2023-01-06 13:39:00 +0100

    GH-168: Spring Boot 3 compliance

--- log-event-spring-boot-starter/src/main/java/no/digdir/logging/event/EventLoggingConfigFactory.java
+++ log-event-spring-boot-starter/src/main/java/no/digdir/logging/event/EventLoggingConfigFactory.java
@@ -1,13 +1,13 @@
 package no.digdir.logging.event;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
 @ConditionalOnClass(EventLogger.class)
 @EnableConfigurationProperties(EventLoggingConfigurationProperties.class)
 class EventLoggingConfigFactory {
